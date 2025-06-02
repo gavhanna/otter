5dbbc5e1fc3c7445e076ccb64bfae832a4ab3e81
Gavin Hanna <gavhanna@gmail.com>
2025-06-02 08:26:57 +0100

    Add admin router for user and recording management with access control

--- apps/server/src/trpc/routers/_app.ts
+++ apps/server/src/trpc/routers/_app.ts
@@ -2,11 +2,13 @@ import { router } from '../trpc';
 import { authRouter } from './auth';
 import { recordingRouter } from './recording';
 import { userRouter } from './user';
+import { adminRouter } from './admin';
 
 export const appRouter = router({
   auth: authRouter,
   recording: recordingRouter,
   user: userRouter,
+  admin: adminRouter,
 });
 
 export type AppRouter = typeof appRouter; 
