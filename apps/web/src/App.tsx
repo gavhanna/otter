38e6a2921cc4e448748a1c4b0eb2fb8f25af48a7
Gavin Hanna <gavhanna@gmail.com>
2025-06-02 16:48:53 +0100

    Refactor TypeScript configuration and enhance Vite setup; implement RecordingInterface component with UI elements for recording management, including button and card components.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,24 +1,5 @@
-import { Outlet } from '@tanstack/react-router';
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css';
+import { RecordingInterface } from "./components/RecordingInterface"
 
-function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-      {/* You can add global layout components here, like a navbar */}
-      {/* <nav>
-        <Link to="/">Home</Link> | {" "}
-        <Link to="/about">About</Link> // Example for a future about route
-      </nav> */}
-      <hr />
-      <Outlet /> {/* This is where child routes will be rendered */}
-      {/* You can also add a global footer here */}
-    </>
-  );
+export default function App() {
+  return <RecordingInterface />
 }
-
-export default App;
