cf1e455f19e2b3c8087a3f5fdec7bc6e60726a56
Gavin Hanna <gavhanna@gmail.com>
2025-11-10 14:17:30 +0000

    Update from deprecated reac router via plugin name

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-vite-plugin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite({
-      routesDirectory: './src/routes'
-    })
-  ],
-  resolve: {
-    dedupe: ['react', 'react-dom']
-  },
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true
-      }
-    }
-  }
+    plugins: [
+        react(),
+        tanstackRouter({
+            routesDirectory: "./src/routes",
+        }),
+    ],
+    resolve: {
+        dedupe: ["react", "react-dom"],
+    },
+    server: {
+        port: 5173,
+        host: "0.0.0.0",
+        proxy: {
+            "/api": {
+                target: "http://localhost:4000",
+                changeOrigin: true,
+            },
+        },
+    },
 });
