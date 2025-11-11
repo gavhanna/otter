07d58cd5f1030ca302e8693cbfb929487621f709
Gavin Hanna <gavhanna@gmail.com>
2025-11-11 09:00:54 +0000

    Removing redunant alias crap from vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-vite-plugin";
-import { createRequire } from "node:module";
-
-const require = createRequire(import.meta.url);
-const reactPath = require.resolve("react");
-const reactJsxPath = require.resolve("react/jsx-runtime");
-const reactDomPath = require.resolve("react-dom");
-const reactDomClientPath = require.resolve("react-dom/client");
 
 export default defineConfig({
     plugins: [
@@ -17,12 +10,6 @@ export default defineConfig({
         }),
     ],
     resolve: {
-        alias: [
-            { find: "react/jsx-runtime", replacement: reactJsxPath },
-            { find: "react-dom/client", replacement: reactDomClientPath },
-            { find: "react-dom", replacement: reactDomPath },
-            { find: "react", replacement: reactPath },
-        ],
         dedupe: ["react", "react-dom"],
     },
     server: {
